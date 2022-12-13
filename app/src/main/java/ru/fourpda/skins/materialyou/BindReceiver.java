344df82fee03397fe7374bae638345730e0f59e8
Pycm <6963578+PycmShoma@users.noreply.github.com>
2022-12-13 17:16:39 +0300

    Add files via upload

--- app/src/main/java/ru/fourpda/skins/materialyou/BindReceiver.java
+++ app/src/main/java/ru/fourpda/skins/materialyou/BindReceiver.java
@@ -1,1 +1,13 @@
-#
+package ru.fourpda.skins.materialyou;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BindReceiver extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+    }
+}
